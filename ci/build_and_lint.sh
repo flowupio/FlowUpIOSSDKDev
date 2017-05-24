ba41cd3b09ceca0d6395b77576ca3b2ce611fdf2
Sergio Gutierrez <sergio@gokarumi.com>
2017-05-24 15:41:08 +0200

    Update CI script to check that the project has been synxed

--- ci/build_and_lint.sh
+++ ci/build_and_lint.sh
@@ -6,5 +6,10 @@ command -v synx >/dev/null 2>&1 || { brew install synx; }
 synx Demo/Demo.xcodeproj
 synx SDK/SDK.xcodeproj
 
+if [ -n "$(git status --porcelain)" ]; then 
+    echo "The project has not been synx'ed"
+    exit -1
+fi
+
 infer run -- xcodebuild -workspace FlowUpIOSSDK.xcworkspace -scheme 'SDK' -destination 'platform=iOS Simulator,name=iPhone 6s Plus' clean build CODE_SIGN_IDENTITY=-
 infer run -- xcodebuild -workspace FlowUpIOSSDK.xcworkspace -scheme 'Demo' -destination 'platform=iOS Simulator,name=iPhone 6s Plus' clean build CODE_SIGN_IDENTITY=-
