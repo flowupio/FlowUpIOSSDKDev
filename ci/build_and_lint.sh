7833972f0a1005b3b3c036a8b59e35f26bd86dab
Sergio Gutierrez <sergio@gokarumi.com>
2017-05-24 15:35:52 +0200

    Run Synx in project

--- ci/build_and_lint.sh
+++ ci/build_and_lint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 command -v infer >/dev/null 2>&1 || { brew install infer; }
+command -v synx >/dev/null 2>&1 || { brew install synx; }
+
+synx Demo/Demo.xcodeproj
+synx SDK/SDK.xcodeproj
 
 infer run -- xcodebuild -workspace FlowUpIOSSDK.xcworkspace -scheme 'SDK' -destination 'platform=iOS Simulator,name=iPhone 6s Plus' clean build CODE_SIGN_IDENTITY=-
 infer run -- xcodebuild -workspace FlowUpIOSSDK.xcworkspace -scheme 'Demo' -destination 'platform=iOS Simulator,name=iPhone 6s Plus' clean build CODE_SIGN_IDENTITY=-
