1b9ae7287cbed279256e38622ceaca80b6a81776
Sergio Gutierrez <sergio@gokarumi.com>
2017-05-24 12:49:03 +0200

    Build SDK project as well

--- ci/build_and_lint.sh
+++ ci/build_and_lint.sh
@@ -2,4 +2,5 @@
 
 command -v infer >/dev/null 2>&1 || { brew install infer; }
 
+infer run -- xcodebuild -workspace FlowUpIOSSDK.xcworkspace -scheme 'SDK' -destination 'platform=iOS Simulator,name=iPhone 6s Plus' clean build CODE_SIGN_IDENTITY=-
 infer run -- xcodebuild -workspace FlowUpIOSSDK.xcworkspace -scheme 'Demo' -destination 'platform=iOS Simulator,name=iPhone 6s Plus' clean build CODE_SIGN_IDENTITY=-
