523e246209dd56e4f63df76d37fb92778e94f932
Sergio Gutierrez <sergio@gokarumi.com>
2017-05-24 19:44:07 +0200

    Separate synx script

--- ci/build_and_lint.sh
+++ ci/build_and_lint.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
 command -v infer >/dev/null 2>&1 || { brew install infer; }
-command -v synx >/dev/null 2>&1 || { gem install synx; }
-
-synx Demo/Demo.xcodeproj
-synx SDK/SDK.xcodeproj
-
-if [ -n "$(git status --porcelain)" ]; then 
-    echo "The project has not been synx'ed"
-    exit -1
-fi
 
 infer run -- xcodebuild -workspace FlowUpIOSSDK.xcworkspace -scheme 'SDK' -destination 'platform=iOS Simulator,name=iPhone 6s Plus' clean build test CODE_SIGN_IDENTITY=-
 infer run -- xcodebuild -workspace FlowUpIOSSDK.xcworkspace -scheme 'Demo' -destination 'platform=iOS Simulator,name=iPhone 6s Plus' clean build CODE_SIGN_IDENTITY=-
