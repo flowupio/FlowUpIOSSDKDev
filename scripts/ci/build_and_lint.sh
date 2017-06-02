24c7242395b4861fdf11bfb86924547ca5a93754
Sergio Gutierrez <sergio@gokarumi.com>
2017-06-02 16:37:01 +0200

    Not build and lint the demo app

--- scripts/ci/build_and_lint.sh
+++ scripts/ci/build_and_lint.sh
@@ -3,4 +3,3 @@
 command -v infer >/dev/null 2>&1 || { brew install infer; }
 
 infer run -- xcodebuild -workspace FlowUpIOSSDK.xcworkspace -scheme 'SDK' -destination 'platform=iOS Simulator,name=iPhone 6s Plus' clean build test CODE_SIGN_IDENTITY=-
-infer run -- xcodebuild -workspace FlowUpIOSSDK.xcworkspace -scheme 'Demo' -destination 'platform=iOS Simulator,name=iPhone 6s Plus' clean build CODE_SIGN_IDENTITY=-
