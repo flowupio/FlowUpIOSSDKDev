7e0969ed70745fbbde4457f3ef66a6a220a63fb3
Sergio Gutierrez <sergio@gokarumi.com>
2017-06-08 10:24:06 +0200

    Update pod version CI script to prevent it from matching wrong version numbers

--- scripts/ci/check_sdk_version.sh
+++ scripts/ci/check_sdk_version.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PODSPEC_VERSION=$(cat SDK/FlowUpIOSSDK.podspec | grep s.version | awk '{print $3}' | tr -d '"')
+PODSPEC_VERSION=$(cat SDK/FlowUpIOSSDK.podspec | grep 's.version\s*=' | awk '{print $3}' | tr -d '"')
 CONFIGURATION_VERSION=$(cat SDK/SDK/Infrastructure/Configuration.h | grep SDKVersion | awk '{print $6}' | tr -d '"@;')
 
 if [ "$PODSPEC_VERSION" != "$CONFIGURATION_VERSION" ]; then
-    echo "SDK versions found in the podspec and the Configuration.h file does not match"
+    echo "SDK versions found in the podspec ($PODSPEC_VERSION) and the Configuration.h file ($CONFIGURATION_VERSION) does not match"
     exit -1
 fi
