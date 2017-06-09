ecdc6e7e573dc6bc6f549ab9b7ac380660fe925c
Sergio Gutierrez <sergio@gokarumi.com>
2017-06-09 17:03:58 +0200

    Increase SDK version to 0.0.2

--- SDK/SDK/Infrastructure/FUPConfiguration.h
+++ SDK/SDK/Infrastructure/FUPConfiguration.h
@@ -13,13 +13,13 @@
 
 static NSTimeInterval const CollectorSchedulerSamplingTimeInterval = 10;
 static NSTimeInterval const ConfigSyncSchedulerFirstReportDelayTimeInterval = 5;
-static NSTimeInterval const ConfigSyncSchedulerTimeBetweenSyncsTimeInterval = HOURS(10);
+static NSTimeInterval const ConfigSyncSchedulerTimeBetweenSyncsTimeInterval = HOURS(6);
 static NSTimeInterval const ConfigSyncSchedulerSyncingTimeInterval = MINUTES(5);
 static NSTimeInterval const ReportSchedulerFirstReportDelayTimeInterval = 15;
 static NSTimeInterval const ReportSchedulerReportingTimeInterval = 15;
 static NSTimeInterval const ReportSchedulerTimeBetweenReportsTimeInterval = MINUTES(5);
 static NSUInteger const MaxNumberOfReportsPerRequest = 898;
-static NSString *const SDKVersion = @"0.0.1";
+static NSString *const SDKVersion = @"0.0.2";
 static NSString *const ApiBaseUrl = @"https://api.flowupapp.com";
 
 #endif /* FUPConfiguration_h */
