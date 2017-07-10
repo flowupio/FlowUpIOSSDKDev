f676b403b1f324f8fad5746911042683c7843298
Sergio Gutierrez <sergio@gokarumi.com>
2017-07-10 11:46:23 +0200

    Add memory benchmark in demo app

--- Demo/Demo/ViewController.m
+++ Demo/Demo/ViewController.m
@@ -10,6 +10,7 @@
 
 @interface ViewController ()
 
+@property (readwrite, nonatomic) NSMutableArray *memoryThings;
 @property (weak, nonatomic) IBOutlet UILabel *cpuUsageLabel;
 
 @end
@@ -18,6 +19,7 @@
 
 - (void)viewDidLoad
 {
+    self.memoryThings = [[NSMutableArray alloc] init];
     [NSTimer scheduledTimerWithTimeInterval:1.0f repeats:YES block:^(NSTimer * _Nonnull timer) {
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
             [self fib];
@@ -30,6 +32,12 @@
     int minStartingValue = 1;
     int maxStartingValue = 40;
     int r = minStartingValue + arc4random_uniform(maxStartingValue - minStartingValue);
+
+    NSLog(@"Storing %d values", r * 1000);
+    for (int i = 0; i < r * 1000; i++) {
+        [self.memoryThings addObject:[[NSObject alloc] init]];
+    }
+
     [self fibonacciOfNumber:r];
 }
 
